41c95bbd45f1620a84ce23aa6a3a06c3a898b5cb
Lucas Borges <lucassborgess@hotmail.com>
2024-03-05 08:49:31 -0300

    apostila02(git-python): funcao 2 implementada

--- menu.py
+++ menu.py
@@ -4,8 +4,11 @@ def menu():
     option = -1
     while option != 0:
         print("1. Somar")
+        print("2. Subtrair")
 
  option = int(input("Escolha uma opção: "))
 
      if option == 1:
             print("Soma")
+        elif option == 2:
+            print("Subtração")
