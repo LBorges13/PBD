082c67b5473fd4c652a870535580470921967f66
Lucas Borges <lucassborgess@hotmail.com>
2024-03-05 08:47:25 -0300

    apostila02(git-python): funcao 1 implementada

---
+++ menu.py
@@ -0,0 +1,11 @@
+menu
+
+def menu():
+    option = -1
+    while option != 0:
+        print("1. Somar")
+
+ option = int(input("Escolha uma opção: "))
+
+     if option == 1:
+            print("Soma")
