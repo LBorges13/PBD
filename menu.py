7e854182313bb4a37461afa12bf3213d93d9ed42
Lucas Borges <lucassborgess@hotmail.com>
2024-03-05 08:51:04 -0300

    apostila02(git-python): funcao 3 4 e 5 implementada

--- menu.py
+++ menu.py
@@ -5,6 +5,11 @@ def menu():
     while option != 0:
         print("1. Somar")
         print("2. Subtrair")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("0. Sair")
+
+
 
  option = int(input("Escolha uma opção: "))
 
@@ -12,3 +17,7 @@ def menu():
             print("Soma")
         elif option == 2:
             print("Subtração")
+        elif option == 3:
+            print("Multiplicação")
+        elif option == 4:
+            print("Divisão")
